366273b7833ac53618709d3a91c3eea2c2ae6a98
vepe99 <viterbogiuseppe1999@libero.it>
2025-02-26 20:41:14 +0100

    unit conversion by setting G=1 and obtain code_time units

--- jdgsim/units.py
+++ jdgsim/units.py
@@ -3,11 +3,16 @@ from astropy import constants as c
 
 class CodeUnits:
 
-    def __init__(self, unit_length, unit_mass, unit_time):
+    def __init__(self, unit_length, unit_mass, G, unit_time=None):
         self.code_length = u.def_unit('code_length', unit_length)
         self.code_mass = u.def_unit('code_mass', unit_mass)
-        self.code_time = u.def_unit('code_unit', unit_time)
-        
+        if unit_time is not None:
+            self.code_time = u.def_unit('code_time', unit_time)
+        else:
+            self.G = c.G.to(self.code_length**3 / self.code_mass / u.s**2) 
+            self.G = self.G * (self.code_mass/self.code_length**3)
+            self.code_time = u.def_unit('code_time', (G/self.G)**(1/2) )
+
 
     
 
