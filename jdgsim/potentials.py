f68485ad620b045c786e4c5b9e7ea6ddc5300b2e
vepe99 <viterbogiuseppe1999@libero.it>
2025-03-03 17:03:18 +0100

    add Miamoto Niagai Potential

--- jdgsim/potentials.py
+++ jdgsim/potentials.py
@@ -8,6 +8,7 @@ from jax import random
 
 NFW_POTENTIAL = 0
 POINT_MASS = 1
+MN_POTENTIAL = 2
 
 
 @partial(jax.jit, static_argnames=['config', 'return_potential'])
@@ -35,7 +36,8 @@ def combined_external_acceleration_vmpa_switch(state, config, params, return_pot
     if return_potential:
         # The POTENTIAL_LIST NEEDS TO BE IN THE SAME ORDER AS THE INTEGER VALUES 
         POTENTIAL_LIST = [lambda state: NFW(state, config=config, params=params, return_potential=True), 
-                        lambda state: point_mass(state, config=config, params=params, return_potential=True)]
+                        lambda state: point_mass(state, config=config, params=params, return_potential=True),
+                        lambda state: MyamotoNagai(state, config=config, params=params, return_potential=True)]
         vmap_function = vmap(lambda i, state: lax.switch(i, POTENTIAL_LIST, state))
         external_acc, external_pot = vmap_function(jnp.array(config.external_accelerations), state_tobe_vmap)
         total_external_acceleration = jnp.sum(external_acc, axis=0)
@@ -43,7 +45,8 @@ def combined_external_acceleration_vmpa_switch(state, config, params, return_pot
         return total_external_acceleration, total_external_potential
     else:
         POTENTIAL_LIST = [lambda state: NFW(state, config=config, params=params, return_potential=False),
-                          lambda state: point_mass(state, config=config, params=params, return_potential=False)]
+                          lambda state: point_mass(state, config=config, params=params, return_potential=False),
+                          lambda state: MyamotoNagai(state, config=config, params=params, return_potential=False)]
         vmap_function = vmap(lambda i, state: lax.switch(i, POTENTIAL_LIST, state))
         external_acc = vmap_function(jnp.array(config.external_accelerations), state_tobe_vmap)
         total_external_acceleration = jnp.sum(external_acc, axis=0)
@@ -128,6 +131,53 @@ def point_mass(state, config, params, return_potential=False):
     
     acc = - params.G * params_point_mass.M * state[:, 0] / (r**3)[:, None]
     pot = - params.G * params_point_mass.M / r
+    
+    if return_potential:
+        return acc, pot
+    else:
+        return acc
+    
+
+@partial(jax.jit, static_argnames=['config', 'return_potential'])
+def MyamotoNagai(state, config, params, return_potential=False):
+    """
+    Compute acceleration of all particles due to a Miyamoto-Nagai profile.
+
+    Parameters
+    ----------
+    state : jnp.ndarray
+        Array of shape (N_particles, 6) representing the positions and velocities of the particles. 
+    config: NamedTuple
+        Configuration parameters.
+    params: NamedTuple
+        Simulation parameters.
+    return_potential: bool
+        If True, also returns the potential energy of the Miyamoto-Nagai profile.
+    
+    Returns
+    -------
+    Tuple
+        - Acceleration: jnp.ndarray 
+            Acecleration of all particles due to Miyamoto-Nagai external potential
+        - Potential: jnp.ndarray
+            Potential energy of all particles due to Miyamoto-Nagai external potential
+            Returned only if return_potential is True.   
+    """
+    params_MN = params.MN_params
+    
+    z2 = state[:, 0, 2]**2
+    b = params_MN.b
+    a = params_MN.a
+
+    Dz = (a+(z2+b**2)**0.5)
+    D = jnp.sum(state[:, 0, :2]**2, axis=1) + Dz**2
+    K = params.G * params_MN.M / D**(3/2)
+    ax = -K * state[:, 0, 0]
+    ay = -K * state[:, 0, 1]
+    az = -K * state[:, 0, 2] * Dz / (z2 + b**2)**0.5
+    acc = jnp.stack([ax, ay, az], axis=1)
+
+    pot = - params.G * params_MN.M / jnp.sqrt(D)
     
     if return_potential:
         return acc, pot
