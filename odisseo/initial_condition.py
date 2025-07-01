a6742a1098e361f851be93c0996e6144091bcece
vepe99 <viterbogiuseppe1999@libero.it>
2025-07-01 12:12:02 +0200

    removed plummer multiprocessing

--- odisseo/initial_condition.py
+++ odisseo/initial_condition.py
@@ -80,42 +80,6 @@ def Plummer_sphere(key: PRNGKeyArray,
      
  
   
-def Plummer_sphere_multiprocess(mass, config, params):
-    """
-    Generate initial conditions for a Plummer sphere using rejection sampling.
-    The calculation is done using numpy.
-
-    Args:
-        mass (float): The total mass of the Plummer sphere.
-        config (NamedTuple): Configuration NamedTuple containing the number of particles (N_particles).
-        params (NamedTuple): Parameters NamedTuple containing:
-    
-    Returns:
-        tuple: A tuple containing:
-            positions (jnp.array): Array of shape (N_particles, 3) representing the positions of the particles.
-            velocities (jnp.array): Array of shape (N_particles, 3) representing the velocities of the particles.
-            masses (jnp.array): Array of shape (N_particles,) representing the masses of the particles.
-    """
-    Plummer_Mtot = params.Plummer_params.Mtot
-    r = np.sqrt( params.Plummer_params.a / (np.random.uniform(size=config.N_particles)**(-3/2) -1))
-    phi = np.random.uniform(size=config.N_particles, low=0, high=2*np.pi) 
-    sin_i = np.random.uniform(size=config.N_particles, low=-1, high=1)
-    
-    positions = np.array([r*np.cos(np.arcsin(sin_i))*np.cos(phi), 
-                          r*np.cos(np.arcsin(sin_i))*np.sin(phi), 
-                          r*sin_i]).T
-    potential = - params.G * Plummer_Mtot / (r**2 + params.Plummer_params.a**2)
-
-    def generate_velocity_Plummer(potential_i, rejection_samples=1000):
-            velocity_i = np.random.uniform(size=(rejection_samples, 3), low=-np.sqrt(-2*potential_i), high=np.sqrt(-2*potential_i))
-            escape_velocity_mask = np.sum(velocity_i**2, axis=1) <= - 2*potential_i
-            isotropic_velocity_mask = np.random.uniform(size=rejection_samples) <= ((0.5 * np.sum(velocity_i**2, axis=1) + potential_i ) / potential_i)**(7/2)
-            return velocity_i[(escape_velocity_mask)&(isotropic_velocity_mask)][0]
-    
-    with Pool(processes=1) as pool:
-        velocities = pool.map(generate_velocity_Plummer, potential)
-    return jnp.array(positions), jnp.array(velocities), 100/config.N_particles*jnp.ones(config.N_particles)
-
 @partial(jax.jit)
 def ic_two_body(mass1: Union[float, jnp.ndarray],
                 mass2: Union[float, jnp.ndarray],
